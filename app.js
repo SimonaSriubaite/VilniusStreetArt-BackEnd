01fbe3a5047e79887710fd7316a5ab7217e7097d
Simona Sriubaite <simona.sriubaite@gmail.com>
2020-09-21 12:02:06 +0300

    mysql connection and database created

--- app.js
+++ app.js
@@ -9,6 +9,28 @@ const app = express();
 app.use(bp.json());
 app.use(cors());
 
+// MySQL connection - creates database if empty
+
+const con = mysql.createConnection(process.env.DB);
+
+con.connect((err) => {
+  if (err) throw err;
+  con.query("SHOW TABLES LIKE 'vsa'", (err, result) => {
+    if (err) console.log(err);
+    if (result.length === 0) {
+      con.query(
+        "CREATE TABLE vsa (id int AUTO_INCREMENT PRIMARY KEY, image TEXT, author TEXT, description TEXT, location TEXT)",
+        (err, result) => {
+          if (err) console.log(err);
+          else console.log("Database created: " + result);
+        }
+      );
+    } else {
+      console.log("Connected to database and it is accessible");
+    }
+  });
+});
+
 app.get("/", (req, res) => {
   res.json(users);
 });
