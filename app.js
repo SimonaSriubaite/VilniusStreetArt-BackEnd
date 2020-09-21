2b72b876edd7cca950b7f227cea2adb205f627b2
Simona Sriubaite <simona.sriubaite@gmail.com>
2020-09-21 12:53:07 +0300

    changes added to getting port

--- app.js
+++ app.js
@@ -44,4 +44,30 @@ app.get("/", (req, res) => {
   });
 });
 
-app.listen(3000, () => console.log("The server is working at port 3000"));
+// POST request to add artwork to database
+
+app.post("/", (req, res) => {
+  if (req.body.image && req.body.location) {
+    con.query(
+      `INSERT INTO vsa (image, author, description, location) VALUES ('${req.body.image}', '${req.body.author}', '${req.body.description}', '${req.body.location}')`,
+      (err, result) => {
+        if (err) {
+          console.log(err);
+          res.status(400).send("Error inserting a task");
+        } else {
+          res.json(result);
+        }
+      }
+    );
+  } else {
+    res.status(400).send("No street art has been passed");
+  }
+});
+
+// Getting port from env file or resorting to default
+
+const port = process.env.PORT || 3000;
+
+// Starting the application on defined port
+
+app.listen(port, () => console.log("The server is running on port: " + port));
